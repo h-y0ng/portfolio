7ca5a57af609d37bafb4563436e0c1fee7ce774a
Inho Yong <inho.yong@vocitec.com>
2019-03-25 21:37:16 -0700

    Updates

---
+++ precache-manifest.e571b03b0cbb07581dd8964cb53b0981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "dce3e0cf687fc42f9bb0",
+    "url": "/portfolio/static/js/main.d21a4476.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/portfolio/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "dce3e0cf687fc42f9bb0",
+    "url": "/portfolio/static/css/main.db5cbb0e.chunk.css"
+  },
+  {
+    "revision": "9b63d98cf3a1b429d579b547607ea28d",
+    "url": "/portfolio/index.html"
+  }
+];
